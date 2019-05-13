0366ad84399a470ea636181ecedf3efadac10581
youcef934 <34548470+youcef934@users.noreply.github.com>
2019-05-13 23:15:39 +0200

    Add files via upload

---
+++ StartDragListener.java
@@ -0,0 +1,10 @@
+package by.naxa.soundrecorder.listeners;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface StartDragListener {
+
+
+    void requestDrag(RecyclerView.ViewHolder viewHolder);
+
+}
